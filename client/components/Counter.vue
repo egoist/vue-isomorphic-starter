116bb93cd132274994f2269aad2ddc110e06ac3a
Lucas Icarus <lucasicarus@hotmail.com>
2016-08-10 23:14:02 +0800

    code-style modification-3

--- client/components/Counter.vue
+++ client/components/Counter.vue
@@ -1,5 +1,5 @@
 <style>
-.counter {
+  .counter {
     cursor: pointer;
     margin: 100px auto;
     border-radius: 3px;
@@ -11,27 +11,27 @@
     background-color: #f0f0f0;
     box-shadow: inset 0 0 10px #ccc;
     user-select: none;
-}
+  }
 </style>
 
 <template>
-<div class='counter' @click='increment'>
+  <div class='counter' @click='increment'>
     {{ count }}
-</div>
+  </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
-export default {
+  import {mapGetters, mapActions} from 'vuex'
+  export default {
     methods: {
-        ...mapActions([
-            'increment'
-        ])
+      ...mapActions([
+        'increment'
+      ])
     },
     computed: {
-        ...mapGetters({
-            count: 'getCount'
-        })
+      ...mapGetters({
+        count: 'getCount'
+      })
     }
-}
+  }
 </script>
--- client/components/Home.vue
+++ client/components/Home.vue
@@ -1,64 +1,64 @@
 <style>
-.container-main {
+  .container-main {
     .logo-vue, .brief {
-        display: flex;
-        width: 100%;
-        align-items: center;
-        justify-content: center;
-        .text {
-            text-align: center;
-        }
+      display: flex;
+      width: 100%;
+      align-items: center;
+      justify-content: center;
+      .text {
+        text-align: center;
+      }
     }
     .features {
-        margin-top: 20px;
-        display: flex;
-        .left,
-        .right {
-            flex: 1;
+      margin-top: 20px;
+      display: flex;
+      .left,
+      .right {
+        flex: 1;
+      }
+      .center {
+        flex: 2;
+      }
+      ul {
+        margin-left: -0.5em;
+        li {
+          margin-top: 15px;
+          margin-bottom: 15px;
         }
-        .center {
-            flex: 2;
-        }
-        ul {
-            margin-left: -0.5em;
-            li {
-                margin-top: 15px;
-                margin-bottom: 15px;
-            }
-        }
+      }
     }
-}
+  }
 </style>
 
 <template lang="html">
-<div class="container-main">
+  <div class="container-main">
     <section class="logo-vue">
-        <div class="center">
-            <img src="../assets/img/logo.png" alt="logo-vue" />
-        </div>
+      <div class="center">
+        <img src="../assets/img/logo.png" alt="logo-vue"/>
+      </div>
     </section>
     <section class="brief">
-        <div class="text">
-            <strong>Vue</strong> server-side rendering in real life. ٩(๑´3‘๑)۶
-            <br>
-            <br>
-             which uses Vue2, Vuex, Vue-router for view and Koa2 for server and Webpack 2 for building.
-        </div>
+      <div class="text">
+        <strong>Vue</strong> server-side rendering in real life. ٩(๑´3‘๑)۶
+        <br>
+        <br>
+        which uses Vue2, Vuex, Vue-router for view and Koa2 for server and Webpack 2 for building.
+      </div>
     </section>
     <section class="features">
-        <div class="left">
-        </div>
-        <div class="center">
-            <h2>Features</h2>
-            <ul>
-                <li>Vue 2 + ES6 + Webpack</li>
-                <li>Koa 2 + ES6 + Webpack</li>
-                <li>Nodemon</li>
-                <li><strong>Hot Reloading</strong></li>
-                <li>Optimized for production use</li>
-            </ul>
-        </div>
-        <div class="right">
-        </div>
-</div>
+      <div class="left">
+      </div>
+      <div class="center">
+        <h2>Features</h2>
+        <ul>
+          <li>Vue 2 + ES6 + Webpack</li>
+          <li>Koa 2 + ES6 + Webpack</li>
+          <li>Nodemon</li>
+          <li><strong>Hot Reloading</strong></li>
+          <li>Optimized for production use</li>
+        </ul>
+      </div>
+      <div class="right">
+      </div>
+  </div>
 </template>
